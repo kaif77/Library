bd7c3ddb728ba9308d017a4dce44f07bdead4efe
sudeshlak <swelikotuwa@gmail.com>
2021-05-15 06:41:26 +0530

    books crud part

--- src/components/Books/BooksList.tsx
+++ src/components/Books/BooksList.tsx
@@ -1,21 +1,40 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
 import Book from "./Book";
+import {IBooks} from "../../types/LibraryTypes";
+type BooksListProps = {
+    bookList: IBooks[]
+    onBookDeleted: (bookIndex:number) => void
+    onUpdateRequest: (bookIndex:number) =>void
+}
+
+const BooksList : React.FC <BooksListProps>= (props) => {
+
+    const renderBookList = () =>{
 
 
-const BooksList : React.FC = () => {
+        if(props.bookList.length===0){
+            return ;
+        }
+        return props.bookList.map((book:IBooks, index:number)=> {
+            return <Book book={book} key={index} index={index+1}
+                         onBookDeleted={props.onBookDeleted}
+                         onUpdateRequest={props.onUpdateRequest}
+            />
+        });
+
+    }
 
     return(
         <Row>
 
-            {/*<Col>
-                {books.length ===0 && <label className='empty-list mb-2'>No books listed here</label>}
-            </Col>*/}
+            <Col>
+                {props.bookList.length ===0 && <label className='empty-list mb-2'>No books listed here</label>}
+            </Col>
 
             <ul className='book-ul'>
-                <Book/>
-                <Book/>
-                <Book/>
+                {renderBookList()}
+
             </ul>
 
         </Row>
