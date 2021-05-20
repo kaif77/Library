ac2c83115aa7da75fb2256560837b7de919c53c2
kaif77 <kaifmuhammed2@gmail.com>
2021-05-20 15:09:05 +0530

    Added Footer for the Page

--- src/views/Library.tsx
+++ src/views/Library.tsx
@@ -3,15 +3,19 @@ import Welcome from '../components/Welcome';
 import LibraryContent from "../components/LibraryContent";
 import {Container} from "react-bootstrap";
 import {ToastProvider} from "react-toast-notifications";
+import Footer from '../components/Footer';
 
 const Library: React.FC = () => {
     return(
+        <React.Fragment>
        <Container fluid={true}>
            <Welcome />
            <ToastProvider>
            <LibraryContent />
            </ToastProvider>
        </Container>
+            <Footer/>
+        </React.Fragment>
           )
 };
 
